4abfd228e4ea9f7dce7dbb451430add9a98fbd11
adilafarhana <adilafarhanavv@gmail.com>
2024-06-13 02:57:43 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AppPharmacy from './component/AppPharmacy';
+import SearchPharmacy from './component/SearchPharmacy';
 
 function App() {
   return (
     <div className="App">
       <h5><AppPharmacy/></h5>
+      <h5><SearchPharmacy/></h5>
     </div>
   );
 }
